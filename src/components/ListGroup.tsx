b2e66a0eaee0b0be822a51dd86d1a037733f39a3
Max <ducthanhnguyen95@gmail.com>
2026-08-11 17:11:44 +0700

    --no-edit

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,14 @@
-import { Fragment } from 'react/jsx-runtime'
-
 function ListGroup() {
-  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
-  return (
-    <>
-      <ul className="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </>
-  )
+    const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
+    return (
+        <>
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default ListGroup
