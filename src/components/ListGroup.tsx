31e8d6568ed8612ec8fa2b65f73106ef10ac8dae
Max <ducthanhnguyen95@gmail.com>
2026-08-11 16:58:03 +0700

    Rendering Lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,16 @@
-import { Fragment } from "react/jsx-runtime"
+import { Fragment } from 'react/jsx-runtime'
 
 function ListGroup() {
-    return (
-        <>
-            <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
-            </ul>
-        </>
-    )
+  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
+  return (
+    <>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default ListGroup
