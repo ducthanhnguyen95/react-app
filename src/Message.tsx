25ace18408e8bed04fe30a425eea0f81a3924f9d
Max <ducthanhnguyen95@gmail.com>
2026-08-11 16:31:11 +0700

    Creating a ListGroup Component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,8 @@
 // PascalCasing
 function Message() {
-    const name = ''
-    if (name)
-        return <h1>Hello {name}</h1>;
-    // JSX: JavaScript XML
-    return <h1>Hello World</h1>;
+  const name = ''
+  if (name) return <h1>Hello {name}</h1>
+  // JSX: JavaScript XML
+  return <h1>Hello World</h1>
 }
-export default Message;
+export default Message
