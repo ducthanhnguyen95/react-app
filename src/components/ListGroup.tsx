ddca3cf399eff4d8a08f15833bd63c8af4ec4ea1
Max <ducthanhnguyen95@gmail.com>
2026-08-12 14:42:38 +0700

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,36 @@
 function ListGroup() {
-    const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
-    return (
-        <>
-            <ul className="list-group">
-                {items.map((item) => (
-                    <li>{item}</li>
-                ))}
-            </ul>
-        </>
-    )
+  let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
+  items = []
+  //   const message = items.length === 0 ? <p>No item found</p> : null
+
+  //   const getMessage = () => {
+  //     return items.length === 0 ? <p>No item found</p> : null
+  //   }
+
+  //   if (items.length === 0)
+  //     return (
+  //       <>
+  //         <h1>List</h1>
+  //         <p>No item found</p>
+  //       </>
+  //     )
+  return (
+    <>
+      <h1>List</h1>
+
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {/* {message} */}
+      {/* {getMessage} */}
+
+      {items.length === 0 && <p>No item found</p>}
+
+      <ul className="list-group">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default ListGroup
