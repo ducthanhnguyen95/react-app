a8581fb77745a2fc82b0f013bec8bc64deb3fc96
Max <ducthanhnguyen95@gmail.com>
2026-08-17 10:36:41 +0700

    Exercise- Building a Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,12 @@
 // }
 // export default App
 
-import Alert from './components/Alert'
+import Button from './components/Button'
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button children="Button" color="primary" onClick={() => console.log('Clicked')} />
     </div>
   )
 }
