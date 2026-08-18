86fb4253e8580f0e1550cb8e7cd65c73448fd55f
Max <ducthanhnguyen95@gmail.com>
2026-08-18 09:38:17 +0700

    Vanilla CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-// import ListGroup from './components/ListGroup'
-// function App() {
-//   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
-//   return (
-//     <div>
-//       <ListGroup items={items} heading="Cities" />
-//     </div>
-//   )
-// }
-// export default App
-
-import { useState } from 'react'
-import Button from './components/Button'
-import Alert from './components/Alert'
-
+import ListGroup from './components/ListGroup/ListGroup'
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false)
+  let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
   return (
     <div>
-      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
-      <Button color="primary" onClick={() => setAlertVisible(true)}>
-        My Button
-      </Button>
+      <ListGroup items={items} heading="Cities" />
     </div>
   )
 }
 export default App
+
+// import { useState } from 'react'
+// import Button from './components/Button'
+// import Alert from './components/Alert'
+
+// function App() {
+//   const [alertVisible, setAlertVisible] = useState(false)
+//   return (
+//     <div>
+//       {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
+//       <Button color="primary" onClick={() => setAlertVisible(true)}>
+//         My Button
+//       </Button>
+//     </div>
+//   )
+// }
+// export default App
